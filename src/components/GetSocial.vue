c88a5850cd66946a4b4057a2d1f366bbc06dca3d
AisAif <Isbahrulfahni@gmail.com>
2024-06-18 22:50:17 +0700

    Update socials

--- src/components/GetSocial.vue
+++ src/components/GetSocial.vue
@@ -12,7 +12,7 @@ const socials = await getSocials()
 </script>
 
 <template>
-    <div class="w-full h-[98%] md:px-6 py-6 grid grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center overflow-y-auto">
+    <div class="w-full h-[98%] md:px-6 py-6 grid grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center overflow-y-auto no-scrollbar">
         <div class="relative w-64 sm:w-full px-2 py-2 flex flex-col justify-center items-center gap-2 rounded bg-white/10 shadow-md border-t-[1.3px] border-r-[1.3px] border-white/50 text-center" v-for="social in socials" :key="social.id">
             <a v-show="social.link" :href="social.link" target="_blank" class="absolute right-2 top-2">
                 <span class="material-symbols-outlined hover:-translate-y-1 hover:translate-x-1 transition-all duration-200">arrow_outward</span>
