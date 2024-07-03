32982d0ad61dcef8ebfc1a97ddc460ab7438bb65
AisAif <Isbahrulfahni@gmail.com>
2024-07-03 12:26:01 +0700

    Change router history

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Project from './views/Project.vue'
@@ -24,7 +24,7 @@ const routes = [
 // keep it simple for now.
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes, // short for `routes: routes`
 })
 
